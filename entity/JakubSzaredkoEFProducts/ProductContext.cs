79695fb0da320d7bfd08e5803e6ec2e42b858d6d
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-04-05 14:30:04 +0200

    Initial commit

---
+++ entity/JakubSzaredkoEFProducts/ProductContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JakubSzaredkoEFProducts;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace JakubSzaredkoEFProducts
+{
+    internal class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Datasource=ProductsDatabase");
+        }
+    }
+}
---
+++ entity/JakubSzaredkoEFProducts/Supplier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JakubSzaredkoEFProducts
+{
+    internal class Supplier
+    {
+        public string CompanyName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+}
