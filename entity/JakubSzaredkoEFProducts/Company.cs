0bada8ea77842a20019ade77c74cdb221efcd1c8
Jakub <jakub.szaredko@gmail.com>
2023-04-23 17:58:56 +0200

    Create `exercise 5` [`lab3`]

---
+++ entity/JakubSzaredkoEFProducts/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JakubSzaredkoEFProducts
+{
+    internal class Company
+    {
+        [Key]
+        public int CompanyID { get; set; }
+        public string CompanyName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
---
+++ entity/JakubSzaredkoEFProducts/Migrations/20230423155408_CompanyBase.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JakubSzaredkoEFProducts.Migrations
+{
+    /// <inheritdoc />
+    public partial class CompanyBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductSupplier_Suppliers_SuppliersSupplierID",
+                table: "ProductSupplier");
+
+            migrationBuilder.RenameColumn(
+                name: "SupplierID",
+                table: "Suppliers",
+                newName: "CompanyID");
+
+            migrationBuilder.RenameColumn(
+                name: "SuppliersSupplierID",
+                table: "ProductSupplier",
+                newName: "SuppliersCompanyID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ProductSupplier_SuppliersSupplierID",
+                table: "ProductSupplier",
+                newName: "IX_ProductSupplier_SuppliersCompanyID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Street",
+                table: "Suppliers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Suppliers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ZipCode",
+                table: "Suppliers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CompanyID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CompanyName = table.Column<string>(type: "TEXT", nullable: false),
+                    Street = table.Column<string>(type: "TEXT", nullable: false),
+                    City = table.Column<string>(type: "TEXT", nullable: false),
+                    ZipCode = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CompanyID);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductSupplier_Suppliers_SuppliersCompanyID",
+                table: "ProductSupplier",
+                column: "SuppliersCompanyID",
+                principalTable: "Suppliers",
+                principalColumn: "CompanyID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductSupplier_Suppliers_SuppliersCompanyID",
+                table: "ProductSupplier");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "ZipCode",
+                table: "Suppliers");
+
+            migrationBuilder.RenameColumn(
+                name: "CompanyID",
+                table: "Suppliers",
+                newName: "SupplierID");
+
+            migrationBuilder.RenameColumn(
+                name: "SuppliersCompanyID",
+                table: "ProductSupplier",
+                newName: "SuppliersSupplierID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ProductSupplier_SuppliersCompanyID",
+                table: "ProductSupplier",
+                newName: "IX_ProductSupplier_SuppliersSupplierID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Street",
+                table: "Suppliers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Suppliers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductSupplier_Suppliers_SuppliersSupplierID",
+                table: "ProductSupplier",
+                column: "SuppliersSupplierID",
+                principalTable: "Suppliers",
+                principalColumn: "SupplierID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
