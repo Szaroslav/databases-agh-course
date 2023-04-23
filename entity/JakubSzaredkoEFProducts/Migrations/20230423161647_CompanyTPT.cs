d4e42d4efff3518b8b5459a711152fa5d4080395
Jakub <jakub.szaredko@gmail.com>
2023-04-23 18:34:25 +0200

    Create `exercise 6` [`lab3`]

---
+++ entity/JakubSzaredkoEFProducts/Migrations/20230423161647_CompanyTPT.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JakubSzaredkoEFProducts.Migrations
+{
+    /// <inheritdoc />
+    public partial class CompanyTPT : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CompanyID",
+                table: "Suppliers",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompanyID",
+                table: "Customers",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    CompanyID = table.Column<int>(type: "INTEGER", nullable: false),
+                    CompanyName = table.Column<string>(type: "TEXT", nullable: false),
+                    Street = table.Column<string>(type: "TEXT", nullable: false),
+                    City = table.Column<string>(type: "TEXT", nullable: false),
+                    ZipCode = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.CompanyID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompanyID",
+                table: "Suppliers",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompanyID",
+                table: "Customers",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
---
+++ entity/JakubSzaredkoEFProducts/Migrations/20230423163139_BackToCompanyTPH.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JakubSzaredkoEFProducts.Migrations
+{
+    /// <inheritdoc />
+    public partial class BackToCompanyTPH : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductSupplier_Suppliers_SuppliersCompanyID",
+                table: "ProductSupplier");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Suppliers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompanyID",
+                table: "Companies",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Companies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductSupplier_Companies_SuppliersCompanyID",
+                table: "ProductSupplier",
+                column: "SuppliersCompanyID",
+                principalTable: "Companies",
+                principalColumn: "CompanyID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductSupplier_Companies_SuppliersCompanyID",
+                table: "ProductSupplier");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Companies");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompanyID",
+                table: "Companies",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CompanyID = table.Column<int>(type: "INTEGER", nullable: false),
+                    City = table.Column<string>(type: "TEXT", nullable: false),
+                    CompanyName = table.Column<string>(type: "TEXT", nullable: false),
+                    Street = table.Column<string>(type: "TEXT", nullable: false),
+                    ZipCode = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CompanyID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Suppliers",
+                columns: table => new
+                {
+                    CompanyID = table.Column<int>(type: "INTEGER", nullable: false),
+                    City = table.Column<string>(type: "TEXT", nullable: false),
+                    CompanyName = table.Column<string>(type: "TEXT", nullable: false),
+                    Street = table.Column<string>(type: "TEXT", nullable: false),
+                    ZipCode = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suppliers", x => x.CompanyID);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductSupplier_Suppliers_SuppliersCompanyID",
+                table: "ProductSupplier",
+                column: "SuppliersCompanyID",
+                principalTable: "Suppliers",
+                principalColumn: "CompanyID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
