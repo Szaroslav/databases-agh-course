ba392f46d5802b6a98092c6a60dba7b94d686fc6
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-04-26 11:48:01 +0200

    Init `Hibernate` ORM [`lab4`]

---
+++ hibernate/project/src/main/java/Main.java
@@ -0,0 +1,37 @@
+import model.Product;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    private static final SessionFactory ourSessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            ourSessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    public static void main(final String[] args) throws Exception {
+        final Session session = getSession();
+        Product product = new Product("Strawberries", 2137);
+        try {
+        } finally {
+            Transaction tx = session.beginTransaction();
+            session.save(product);
+            tx.commit();
+            session.close();
+        }
+    }
+}
